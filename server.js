8cb07d8f6c40e03a0f2dc5dddb531f08d5618f3b
Jinkc21 <jin.k.choe@gmail.com>
2024-04-24 18:18:38 -0400

    added data to seeds and started initial code for prompts

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-const express = require('express');
-// Import and require mysql2
+// Import and require mysql2 and inquirer
 const mysql = require('mysql2');
+const inquirer = require("inquirer");
+require("console.table");
 
 const PORT = process.env.PORT || 3001;
-const app = express();
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
   {
     host: 'localhost',
+    port: 3306,
     user: 'root',
     password: '',
     database: 'employees_db'
@@ -20,16 +18,90 @@ const db = mysql.createConnection(
   console.log(`Connected to the employees_db database.`)
 );
 
+db.connect((err) => {
+  if (err) throw err;
+// initializes prompts once connection is made
+  console.log("MySql Connected");
+  init();
+});
 
+// function to start initial prompt
+function init(){
+  inquirer.prompt([
+  {
+    type: 'list',
+    name:'toDo',
+    message: 'What would you like to do?',
+    choices: [
+    'View All Employees',
+    'Add Employee',
+    'View Employees By Manager',
+    'Update Employee Managers',
+    'View All Roles',
+    'Update Employee Role',
+    'Add Role',
+    'View All Departments',
+    'View Employees By Department',
+    'Add Department',
+    'Delete Departments, Roles, and Employees',
+    'Quit'
+    ]
+  }
+  ])
+  .then((res)=>{
+    console.log(res.userChoice);
+    switch(res.userChoice){
+      case 'View All Employees':
+        viewAllEmployees();
+        break;
+      case 'Add Employee':
+        addEmployee();
+        break;
+      case 'View Employees By Manager':
+        viewEmployeesByManager();
+        break;
+      case 'Update Employee Managers':
+        updateEmployeeManagers();
+        break;
+      case 'View All Roles':
+        viewAllRoles();
+        break;        
+      case 'Update Employee Role':
+        updateEmployeeRole();
+        break;
+      case 'Add Role':
+        addRole();
+        break;
+      case 'View All Departments':
+        viewAllDepartments();
+        break;
+      case 'View Employees By Department':
+        viewEmployeesByDepartment();
+        break;
+      case 'Add Department':
+        addDepartment();
+        break;
+      case 'Delete Departments, Roles, and Employees':
+        deleteDepartmentsRolesEmployees();
+        break;  
+      case 'Quit':
+        connection.end();
+        break;
+      }
+      
+    }).catch((err)=>{
+  if(err)throw err;
+  });
+}
 
 
 
 // Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-  });
+// app.use((req, res) => {
+//     res.status(404).end();
+//   });
   
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+//   app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+//   });
   
